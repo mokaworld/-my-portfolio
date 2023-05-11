c058233d9861c1dfa9a85bcfeb9f48cf91036849
ahmed abd ellatif <129755055+mokaworld@users.noreply.github.com>
2023-05-11 17:45:51 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+/* START OWL CAROUSEL */
+$('.owl-carousel').owlCarousel({
+    rtl: true,
+    loop: true,
+    margin: 10,
+    nav: false,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+})
+
+/* END OWL CAROUSEL */
